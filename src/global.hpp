27d95216966949abed7abdbea472d2f230257cb6
Liviu <liviu-andrei.arsenescu@he-arc.ch>
2024-05-29 13:53:45 +0200

    fix(src): some quality of life mods, including comments and code refactoring

--- src/global.hpp
+++ src/global.hpp
@@ -3,6 +3,16 @@
 #include <string>
 #include <cstdint>
 
+/* 
+ * All the general constants that i will use in the game
+ * ## sprite_scale     ## -> the mutiplier that the sprite will have compared to the base sprite
+ * ## sprite_size      ## -> the size of the base sprite in pixels
+ * ## rect_size        ## -> the size of the sprite after the scale
+ * ## tile_grid_width  ## -> the number of rectangles the window will contain, horizontally
+ * ## tile_grid_height ## -> the number of rectangles the window will contain, vertically
+ * ## window_width     ## -> the width of the window in pixels
+ * ## window_height    ## -> the height of the window in pixels
+ */
 constexpr int frame_rate = 60;
 constexpr float sprite_scale = 3;
 constexpr int sprite_size = 16;
@@ -14,6 +24,13 @@ constexpr int tile_grid_height = 18;
 constexpr int window_width = tile_grid_width * sprite_size * sprite_scale;
 constexpr int window_height = tile_grid_height * sprite_size * sprite_scale;
 
+/*
+ * Enum that represents the direction of an entity
+ * the numbers assigned will be used, for example, 
+ * in computing the sprite rectangle from the sprite sheet, 
+ * or define node neighbors
+*/
+
 enum class Direction {
     UP = 0,
     DOWN = 1,
@@ -22,4 +39,8 @@ enum class Direction {
     NONE = -1
 };
 
+/* 
+ * A funciton that checks if two provided directions are different,
+ * used by various objects troughout the game
+ */
 bool isOppositeDirection(const Direction& d1, const Direction& d2);
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 
 #include <iostream>
 
+/*
+ * ARCMAN -> C++ Object-Oriented Programming project
+ *
+ * ### Liviu-Andrei Arsenescu, first year ISC, IL ###
+ *
+ * Project made during the OOP C++ course at 
+ * HE-ARC Neuchâtel, Switzerland
+ */
+
+/*
+ * The main function will be used as a manager
+ * for the "state machine" of the game instance
+ */
+
 int main() {
     Game& game = Game::getInstance();
 
@@ -9,11 +23,12 @@ int main() {
 
     try {
         game.loadTextures();
-        game.gameLoop();
     } catch(CustomException& e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
+
+    game.gameLoop();
 
     return 0;
 }
